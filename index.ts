d76b551ad6ff02de061fab04f208d57cede6c2b2
Masoud Ghorbani <msud.ghorbani@gmail.com>
2018-09-24 21:23:30 +0330

    create index.ts as package entry point

---
+++ index.ts
@@ -0,0 +1,5 @@
+import CircularBuffer from './src/modules/circularbuffer';
+import PersistableCircularBuffer from './src/modules/persistable_circularbuffer';
+
+// export classes
+export { CircularBuffer, PersistableCircularBuffer };
