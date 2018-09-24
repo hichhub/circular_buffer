a05972ff72a5914c7c49234854579d992ba0f6f7
Masoud Ghorbani <msud.ghorbani@gmail.com>
2018-09-24 17:26:49 +0330

    add get test case

--- tests/circularbuffer.spec.ts
+++ tests/circularbuffer.spec.ts
@@ -1,14 +1,18 @@
 import CircularBuffer from '../src/modules/circularbuffer';
 
 describe('CircularBuffer', () => {
-  let CB = new CircularBuffer(100)
+  let CB = new CircularBuffer(100);
   let dummykv = {
     key: 'testName',
     value: 'testValue'
-  }
+  };
 
   test('expect to set new key-value', async () => {
-    CB.set(dummykv.key, dummykv.value)
+    CB.set(dummykv.key, dummykv.value);
     expect(await CB.get(dummykv.key)).toBe(dummykv.value)
+  });
+
+  test('expect to get key\'s value', async () => {
+    expect(await CB.get(dummykv.key)).toBe(dummykv.value);
   });
 });
