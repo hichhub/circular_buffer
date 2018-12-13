f16558ca44a956591c2a82f0a6f6d5de0d2b89d9
Ehsan <e.hosseini.ir@gmail.com>
2018-12-13 16:35:45 +0330

    change logger to console"

--- src/logger.js
+++ src/logger.js
@@ -2,7 +2,4 @@
 exports.__esModule = true;
 var pino = require("pino");
 var constants_1 = require("./constants");
-exports.logger = pino({
-    level: constants_1["default"].LOG_LEVEL,
-    name: constants_1["default"].APP_ID
-});
+exports.logger = console;
--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,4 @@
 import * as pino from 'pino';
 import constants from './constants';
 
-export const logger = pino({
-  level: constants.LOG_LEVEL,
-  name: constants.APP_ID,
-});
+export const logger = console;
